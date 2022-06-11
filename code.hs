fab54dd147ba5734437c959548784bfd37dd80a2
firwanaa <firwanaa@sheridancollege.ca>
2022-06-11 04:54:39 -0400

    match function.

--- code.hs
+++ code.hs
@@ -1,5 +1,3 @@
-module Main where
-import Data.List
 import System.IO
 
 hangman :: IO ()
@@ -10,15 +8,27 @@ hangman = do putStrLn "Think of a word:"
 
 sgetLine = do x <- getCh
               if x == '\n' then
-                do putChar x
-                     return []
+                 do putChar x
+                    return []
               else
-                do putChar '-'
-                   xs <- sgetLine
-                   return (x:xs)
+                 do putChar '-'
+                    xs <- sgetLine
+                    return (x:xs)
 
 getCh :: IO Char
 getCh = do hSetEcho stdin False
            x <- getChar
            hSetEcho stdin True
            return x
+
+play :: String -> IO ()
+play word = do putStr "? "
+               guess <- getLine
+               if guess == word then
+                  putStrLn "You got it!!"
+               else
+                  do putStrLn (match word guess)
+                     play word
+
+match :: String -> String -> String
+match xs ys = [if elem x ys then x else '-' | x <- xs]
